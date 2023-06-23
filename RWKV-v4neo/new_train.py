83adb5c45a178eecd02cac9d678994d5a5292b08
picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-06-24 01:11:44 +0800

    WIP infctx-validation

--- RWKV-v4neo/new_train.py
+++ RWKV-v4neo/new_train.py
@@ -3,10 +3,12 @@ from lightning.pytorch.cli import LightningCLI
 from src.model import RWKV
 from src.data import get_data_module
 
+# Locking the seed, for better replication of train runs
+from lightning.fabric import seed_everything
+seed_everything(3941088705)
 
 def cli_main():
     LightningCLI(RWKV, get_data_module, save_config_kwargs={"overwrite": True})
-
 
 if __name__ == "__main__":
     cli_main()
