2e61c50a12ae4cadc9d010a87365fdffab9af82e
picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-07-02 21:35:30 +0800

    lets redo the foundation training

--- RWKV-v4neo/new_train.py
+++ RWKV-v4neo/new_train.py
@@ -3,10 +3,6 @@ from lightning.pytorch.cli import LightningCLI
 from src.model import RWKV
 from src.data import get_data_module
 
-# Locking the seed, for better replication of train runs
-from lightning.fabric import seed_everything
-seed_everything(3941088705)
-
 def cli_main():
     LightningCLI(RWKV, get_data_module, save_config_kwargs={"overwrite": True})
 
